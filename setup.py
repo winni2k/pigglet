673e7a29e3930ac0e37ee7f622a497b49525cc1e
Warren Kretzschmar <warrenk@kth.se>
2019-08-15 13:22:26 +0200

    Setup tox

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pigglet',
+    version='0.1.0',
+    description='The Philogenetic Inference and genotyping from Genotype Likelihoods Tool',
+    author='Warren Kretzschmar',
+    author_email='warren.kretzschmar@ki.se',
+    url='https://github.com/winni2k',
+    package_dir = {'': 'src'},
+    packages=['pigglet'],
+)
