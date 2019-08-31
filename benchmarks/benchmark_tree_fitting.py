3172148265fae5fab9bc98b85c820b9bd8ad7ba8
Warren Kretzschmar <warrenk@kth.se>
2019-08-31 19:29:05 +0200

    Add more tests

--- benchmarks/benchmark_tree_fitting.py
+++ benchmarks/benchmark_tree_fitting.py
@@ -5,7 +5,7 @@ import networkx as nx
 from pigglet_testing.builders.tree_likelihood import MCMCBuilder
 
 
-def test_arbitrary_trees(n_burnin_iter=10, n_mutations=10):
+def test_arbitrary_trees_all_mutations_have_sample(n_burnin_iter=10, n_mutations=10):
     # given
     rand_g = nx.gnr_graph(n_mutations, 0).reverse()
     nx.relabel_nodes(rand_g, {n: n - 1 for n in rand_g}, copy=False)
@@ -21,30 +21,65 @@ def test_arbitrary_trees(n_burnin_iter=10, n_mutations=10):
 
     # set GLs to het for every mutation of the sample and to hom ref for all other mutations
     for sample, attachment_point in enumerate(filter(lambda n: n != -1, rand_g)):
-        mutations = set(nx.ancestors(rand_g, attachment_point))
-        mutations.add(attachment_point)
-        mutations.remove(-1)
-        for mutation in mutations:
-            b.with_mutated_gl_at(sample, mutation)
-        for non_mutation in set(rand_g) - mutations:
-            if non_mutation != -1:
-                b.with_unmutated_gl_at(sample, non_mutation)
+        add_gl_at_ancestor_mutations_for(attachment_point, b, rand_g, sample)
 
     mcmc = b.build()
 
     return mcmc
 
 
+def test_arbitrary_trees_leaves_have_sample(n_burnin_iter=10, n_mutations=10):
+    # given
+    rand_g = nx.gnr_graph(n_mutations, 0).reverse()
+    nx.relabel_nodes(rand_g, {n: n - 1 for n in rand_g}, copy=False)
+
+    leaf_nodes = [x for x in rand_g.nodes() if rand_g.out_degree(x) == 0]
+
+    b = MCMCBuilder()
+    b.with_n_burnin_iter(n_burnin_iter)
+    b.with_n_sampling_iter(0)
+
+    # set GLs to het for every mutation of the sample and to hom ref for all other mutations
+    for sample, attachment_point in enumerate(leaf_nodes):
+        add_gl_at_ancestor_mutations_for(attachment_point, b, rand_g, sample)
+
+    mcmc = b.build()
+
+    return mcmc
+
+
+def add_gl_at_ancestor_mutations_for(attachment_point, b, rand_g, sample):
+    mutations = set(nx.ancestors(rand_g, attachment_point))
+    mutations.add(attachment_point)
+    mutations.remove(-1)
+    for mutation in mutations:
+        b.with_mutated_gl_at(sample, mutation)
+    for non_mutation in set(rand_g) - mutations:
+        if non_mutation != -1:
+            b.with_unmutated_gl_at(sample, non_mutation)
+
+
 if __name__ == '__main__':
     import timeit
 
-    # mcmc = test_arbitrary_trees(10)
+    # m (samples) == n (sites)
     repeats = 3
+    print('Testing random tree with leaf nodes have sample')
+    for number, mutations in [(10, 100), (10, 300), (10, 1000), (10, 3000), (1000, 10)]:
+        print(f'number={number}, repeats={repeats}, mutations={mutations}')
+        timings = timeit.repeat('mcmc.calc.sample_marginalized_log_likelihood()',
+                                number=number,
+                                repeat=repeats,
+                                globals=globals(),
+                                setup=f'mcmc = test_arbitrary_trees_leaves_have_sample(n_mutations={mutations})')
+        print(mean(timings), stdev(timings))
+
+    print('Testing random tree with every mutation has sample')
     for number, mutations in [(10, 100), (10, 300), (2, 1000), (1, 3000), (1000, 10)]:
         print(f'number={number}, repeats={repeats}, mutations={mutations}')
         timings = timeit.repeat('mcmc.calc.sample_marginalized_log_likelihood()',
                                 number=number,
                                 repeat=repeats,
                                 globals=globals(),
-                                setup=f'mcmc = test_arbitrary_trees(n_mutations={mutations})')
+                                setup=f'mcmc = test_arbitrary_trees_all_mutations_have_sample(n_mutations={mutations})')
         print(mean(timings), stdev(timings))
