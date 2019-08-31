d215412b4c81de08354f5fc80f63847a27742ae2
Warren Kretzschmar <warrenk@kth.se>
2019-08-31 20:59:10 +0100

    Load two sites

--- tests/test_acceptance/test_gl_loading.py
+++ tests/test_acceptance/test_gl_loading.py
@@ -36,18 +36,39 @@ def site_pl_iter(records):
         yield site_info, values
 
 
-def test_loads_gls_of_single_site_and_two_samples_from_vcf(tmpdir):
-    vcf_file = tmpdir.join('input.vcf')
-    with open(vcf_file, 'w') as fh:
-        fh.write("##fileformat=VCFv4.2\n")
-        fh.write(
-            '##FORMAT=<ID=PL,Number=G,Type=String,Description="Phred scaled likelihood">\n')
-        fh.write(
-            "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\tsample_1\tsample_2\n")
-        fh.write('20\t14370\t.\tG\tA\t29\tPASS\t.\tPL\t1,2,4\t3,7,8\n')
+class VCFBuilder:
+    def __init__(self, tmpdir):
+        self.vcf_file = tmpdir.join('input.vcf')
+        self.gls = []
 
-    loader = LikelihoodLoader()
-    loader.load(vcf_file)
+    def build(self):
+        with open(self.vcf_file, 'w') as fh:
+            fh.write("##fileformat=VCFv4.2\n")
+            fh.write(
+                '##FORMAT=<ID=PL,Number=G,Type=String,Description="Phred scaled likelihood">\n')
+            fh.write(
+                "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\tsample_1\tsample_2\n")
+            for idx, site_gls in enumerate(self.gls):
+                row = f'20\t{idx + 1}\t.\tG\tA\t29\tPASS\t.\tPL'
+                for tripple in site_gls:
+                    tripple = [str(v) for v in tripple]
+                    row += '\t' + ','.join(tripple)
+                row += '\n'
+                fh.write(row)
+        loader = LikelihoodLoader()
+        loader.load(self.vcf_file)
+        return loader
+
+    def with_site_gls(self, gls):
+        self.gls.append(gls)
+        return self
+
+
+def test_loads_gls_of_single_site_and_two_samples_from_vcf(tmpdir):
+    b = VCFBuilder(tmpdir)
+    b.with_site_gls([[1, 2, 4], [3, 7, 8]])
+    loader = b.build()
+
     assert loader.gls.shape == (1, 2, 3)
     first_row = loader.gls[0]
     assert first_row[0][0] == 1
@@ -56,3 +77,19 @@ def test_loads_gls_of_single_site_and_two_samples_from_vcf(tmpdir):
     assert first_row[1][0] == 3
     assert first_row[1][1] == 7
     assert first_row[1][2] == 8
+
+
+def test_loads_gls_of_two_sites_and_two_samples_from_vcf(tmpdir):
+    b = VCFBuilder(tmpdir)
+    b.with_site_gls([[1, 2, 3], [4, 5, 6]])
+    b.with_site_gls([[7, 8, 9], [10, 11, 12]])
+    loader = b.build()
+
+    assert loader.gls.shape == (2, 2, 3)
+    first_row = loader.gls[1]
+    assert first_row[0][0] == 7
+    assert first_row[0][1] == 8
+    assert first_row[0][2] == 9
+    assert first_row[1][0] == 10
+    assert first_row[1][1] == 11
+    assert first_row[1][2] == 12
