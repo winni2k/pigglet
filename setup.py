26ef29265c1fb2f819a1e74221a451fe69192a91
Warren Kretzschmar <warrenk@kth.se>
2019-09-01 09:29:46 -0400

    Adds cli interface

--- setup.py
+++ setup.py
@@ -27,8 +27,12 @@ setup(
     author='Warren Kretzschmar',
     author_email='warren.kretzschmar@ki.se',
     url='https://github.com/winni2k',
-    install_requires=['networkx', 'numpy', 'pysam'],
+    install_requires=['networkx', 'numpy', 'pysam', 'click'],
     packages=find_packages('src', exclude=['contrib', 'docs', 'tests', 'test_utils']),  # Required
     python_requires='>=3.6, <4',
     package_dir={'': 'src'},
+    entry_points='''
+    [console_scripts]
+    pigglet=pigglet.cli:cli
+    ''',
 )
