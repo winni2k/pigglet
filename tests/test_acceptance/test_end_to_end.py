2b686b468f4eb45073af833e38e1c355999693f7
Warren Kretzschmar <warrenk@kth.se>
2019-09-01 08:37:53 -0400

    Use GML as output for now

--- tests/test_acceptance/test_end_to_end.py
+++ tests/test_acceptance/test_end_to_end.py
@@ -1,5 +1,6 @@
 import subprocess
 
+import networkx as nx
 import pytest
 
 from pigglet_testing.builders.vcf import VCFBuilder
@@ -20,12 +21,12 @@ def test_single_mutation_one_sample(tmpdir):
     b = VCFBuilder(tmpdir)
     b.with_site_gls([0, 1, 0])
     vcf_file = b.build()
-    prefix = tmpdir / 'out.'
+    prefix = tmpdir / 'out'
     r = CLIRunner(vcf_file=vcf_file, prefix=prefix)
-    out_newick = str(prefix) + 'nw'
+    out_gml = str(prefix) + '.gml'
 
     # when
     r.run()
 
     # then
-    assert open(out_newick).read() == '(-1, 0)'
+    assert nx.read_gml(out_gml).edges == [(-1, 0)]
