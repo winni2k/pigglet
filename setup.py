22fdde5575579d4b3c4dcd8e3487ffe3729a7249
Warren Kretzschmar <warren.kretzschmar@ki.se>
2020-07-30 23:05:52 +0200

    Bump to v0.12.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
         "The Phylogenetic Inference and genotyping from"
         " Genotype Likelihoods Tool"
     ),
-    version="0.12.1",
+    version="0.12.2",
     classifiers=[  # Optional
         # How mature is this project? Common values are
         #   3 - Alpha
